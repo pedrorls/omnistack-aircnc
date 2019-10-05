2f1523df6e55b0f2aadec8124b5323703937cab9
Pedro Silva <pedro.rls93@gmail.com>
2019-10-05 16:16:10 -0300

    connected with mongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello world!" });
-});
+mongoose.connect(
+  "mongodb+srv://pedrorls:pedrorls@cluster0-brlwe.mongodb.net/admin?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
