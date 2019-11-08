367a246e41882c1c982bc3978ae77ac5c97f246d
Pedro Silva <pedro.rls93@gmail.com>
2019-11-07 23:41:27 -0300

    add bookings store view

---
+++ backend/src/controllers/BookingController.js
@@ -0,0 +1,23 @@
+const Booking = require("../models/Booking");
+
+module.exports = {
+  async store(request, response) {
+    const { user_id } = request.headers;
+    const { spot_id } = request.params;
+    console.log(spot_id);
+    const { date } = request.body;
+
+    const booking = await Booking.create({
+      user: user_id,
+      spot: spot_id,
+      date
+    });
+
+    await booking
+      .populate("user")
+      .populate("spot")
+      .execPopulate();
+
+    return response.json(booking);
+  }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
 const DashboardController = require("./controllers/DashboardController");
+const BookingControllers = require("./controllers/BookingController");
 const uploadConfig = require("./config/upload");
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -14,5 +15,7 @@ routes.post("/sessions", SessionController.store);
 
 routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+
+routes.post("/spots/:spot_id/bookings", BookingControllers.store);
 
 module.exports = routes;
